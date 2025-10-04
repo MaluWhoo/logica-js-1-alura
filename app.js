44ff1cb1e6d5f21641404ccd7a3ca99e774f0481
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-10-04 15:15:17 -0300

    Update JS

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 alert("Bem vindo ao jogo do número secreto!");
-let numeroSecreto = 5;
-console.log(`O número secreto é: ${numeroSecreto}`);
+let numeroSecreto = parseInt(Math.random() * 100 + 1) ; // Gera um número aleatório
 let chute;
 let tentativas = 1;
 
 // enquanto o chute não for igual ao Número Secreto:
 while (chute != numeroSecreto){
-    chute = prompt(`Escolha um número entre 1 a 10`);
+    chute = prompt(`Escolha um número entre 1 a 100`);
 
     // Se o chute for = numeroSecreto:
     if (chute == numeroSecreto){
-        alert(`Você acertou! O número secreto era: ${numeroSecreto}. Você tentou ${tentativas}`);
+        break;
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
@@ -21,3 +20,13 @@ while (chute != numeroSecreto){
         tentativas ++;
     }
 }
+
+// OPERADOR TERNÁRIO
+let palavraTentativa = tentativas > 1 ? "tentativas":"tentativa" ;
+alert(`Você acertou! O número secreto era: ${numeroSecreto}. Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`);
+
+// if(tentativas > 1){
+//     
+// } else {
+//     alert(`Você acertou! O número secreto era: ${numeroSecreto}. Você tentou ${tentativas} tentativa.`);
+// }
