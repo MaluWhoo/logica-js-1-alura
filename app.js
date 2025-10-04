e6e939a7a0962dc0b869a1ac59a6a21fd24b2c4c
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-10-04 14:11:24 -0300

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert("Bem vindo ao jogo do número secreto!");
 let numeroSecreto = 5;
 console.log(`O número secreto é: ${numeroSecreto}`);
 let chute;
+let tentativas = 1;
 
 // enquanto o chute não for igual ao Número Secreto:
 while (chute != numeroSecreto){
@@ -9,12 +10,14 @@ while (chute != numeroSecreto){
 
     // Se o chute for = numeroSecreto:
     if (chute == numeroSecreto){
-        alert(`Você acertou! O número secreto era: ${numeroSecreto}`);
+        alert(`Você acertou! O número secreto era: ${numeroSecreto}. Você tentou ${tentativas}`);
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O númerp secreto é maior que ${chute}`);
         }
+
+        tentativas ++;
     }
 }
